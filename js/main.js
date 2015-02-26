ab13582c476c2f3519b6ad3ae4efc1a640ecd22f
unknown <ivicabatinic123@gmail.com>
2015-02-26 22:13:53 +0100

    završen header, navigacija i footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 (function ($) {
+    /**
+    * Menu button click event
+    */
+    $('#menu-button').click(function() {
+        $('.inner-wrapper').toggleClass('open');
+    });
 
+    /**
+    * Search button click event
+    */
+    $('#search-button').click(function() {
+        $('.search').toggleClass('open');
+    });
 })(jQuery);
